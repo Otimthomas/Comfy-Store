c0ac6de59ec5b11c7ce2b7883a2bf5d5c63c310a
Thomas Otim <totim444@hotmail.co.uk>
2022-01-28 09:24:41 +0300

    CartItem, cartColumns, cartTotals and cartContent components complete

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,14 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+
+  return cart ? JSON.parse(cart) : [];
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 535,
@@ -34,6 +40,10 @@ export const CartProvider = ({ children }) => {
 
   // clear cart
   const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <CartContext.Provider value={{ ...state, addToCart }}>
