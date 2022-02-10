182ed86eb04ba2063fb1744044fd841c05e481c3
Thomas Otim <totim444@hotmail.co.uk>
2022-02-10 12:39:15 +0300

    login and logout functionality using Auth0 completed

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,18 +3,20 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
-  const { isAuthenticated, loginWithRedirect, logout, user, isloading } =
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
     useAuth0();
 
   const [myUser, setMyUser] = useState(null);
 
   useEffect(() => {
-    console.log(`user : ${user}`);
-    console.log(`isAuthenitcated : ${isAuthenticated}`);
-    console.log(`isLoading : ${isloading}`);
+    if (isAuthenticated) {
+      setMyUser(user);
+    } else {
+      setMyUser(null);
+    }
   }, [isAuthenticated]);
   return (
-    <UserContext.Provider value={(loginWithRedirect, logout, myUser)}>
+    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
       {children}
     </UserContext.Provider>
   );
